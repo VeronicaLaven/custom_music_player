640a2904b5592b8d33553ea9bdc7ccfd83c3f916
Veronica Laven <vatlaven@Veronicas-MacBook-Pro.local>
2023-01-26 17:16:28 -0700

    updated playthis.py

--- playthis.py
+++ playthis.py
@@ -21,85 +21,136 @@ class Media_Player:
 
         # set TKinter window geometry
         self.window = Tk()
+        self.window.rowconfigure(0, weight=4)
+        self.window.rowconfigure(1, weight=1)
+        self.window.rowconfigure(2, weight=1)
+        self.window.rowconfigure(3, weight=1)
+        self.window.rowconfigure(4, weight=1)
+        self.window.rowconfigure(5, weight=1)
+        self.window.rowconfigure(6, weight=1)
+        self.window.columnconfigure(5, weight=2)
+        self.window.columnconfigure(4, weight=2)
+        self.window.columnconfigure(3, weight=2)
+        self.window.columnconfigure(2, weight=2)
+        self.window.columnconfigure(1, weight=2)
+        self.window.columnconfigure(0, weight=2)
+        
+
         self.window.geometry("400x400")
 
-        # Create a menu bar
+        # set background color
+        self.window.grid_propagate(0)
+        self.window.configure(bg='maroon')
+
+        # create a top menu bar
         menubar = Menu(self.window)
         file_menu = Menu(menubar, tearoff=0)
         file_menu.add_command(label="Exit", command=self.window.destroy)
         menubar.add_cascade(label="File", menu=file_menu)
         self.window.config(menu=menubar)
 
+        # Create a top heading label widget with text "My Media Player"
+        top_header = Label(self.window, text="My Media Player", font=("Arial", 24), fg="white", bg="maroon", pady=20, highlightbackground="maroon", highlightthickness=0)
+        # Pack the label widget
+        top_header.pack()
+        
 
-        # Create a canvas to display the background image
-        self.canvas = Canvas(self.window, bg="blue")
-        self.canvas.pack(expand=YES, fill=BOTH)
+ # TKinter Buttons for each of the radio stations to play and pause / stop
 
-        # Load the image
-        img = Image.open("images/radio.png")
-        # Resize the image
-        img = img.resize((400, 400), Image.ANTIALIAS)
-        # Convert the image to PhotoImage object
-        self.bg_image = ImageTk.PhotoImage(img)
+# ============================ KUSC Buttons 
+        # Button to play KUSC classical
+        self.play_button_kusc_classical = Button(self.window, text="Play KUSC", command=self.play_kusc_classical, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.play_button_kusc_classical.grid(row=1, column=1, ipadx=0, ipady=0, sticky="ne")
 
-        # Set the background image of the canvas
-        self.canvas.create_image(0, 0, image=self.bg_image, anchor=NW, tags="bg_img")
-        self.canvas.lower("bg_img")
+        kusc_image = Image.open("images/kusccal.png")
+        kusc_image = kusc_image.resize((60, 30), Image.ANTIALIAS)
+        self.kusc_image = ImageTk.PhotoImage(kusc_image)
+        self.kusc_image_label = Label(self.window, image=self.kusc_image, borderwidth=0, highlightthickness=0)
+        self.kusc_image_label.grid(row=1, column=2, ipadx=0, ipady=0, sticky="n")
+      
 
+        # Button to stop KUSC Classical
+        self.stop_button_kusc_classical = Button(self.window, text="Stop KUSC", command=self.stop_kusc_classical, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.stop_button_kusc_classical.grid(row=1, column=3, ipadx=0, ipady=0, sticky="nw")
 
+# ============================ MN Buttons 
+         # Button to play MN Classical
+        self.play_button_mn_classical = Button(self.window, text="Play MN", command=self.play_mn_classical, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.play_button_mn_classical.grid(row=2, column=1, ipadx=0, ipady=0, sticky="ne")
 
-    # TKinter Buttons for each of the radio stations to play and pause / stop
+# Load the image
+        mn_image = Image.open("images/yourclassical1.png")
+        mn_image = mn_image.resize((60, 30), Image.ANTIALIAS)
+        self.mn_image = ImageTk.PhotoImage(mn_image)
+        self.mn_image_label = Label(self.window, image=self.mn_image, borderwidth=0, highlightthickness=0)
+        self.mn_image_label.grid(row=2, column=2, ipadx=0, ipady=0, sticky="n")
 
-        # Button to play and pause KUSC classical music
-        self.play_button_kusc_classical = Button(self.window, text="Play", command=self.play_kusc_classical)
-        self.play_button_kusc_classical.place(x=40, y=60)
+        # Button to stop MN Classical
+        self.stop_button_mn_classical = Button(self.window, text="Stop MN", command=self.stop_mn_classical, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.stop_button_mn_classical.grid(row=2, column=3, ipadx=0, ipady=0, sticky="nw")
 
-        # Load the image
-        img = Image.open("images/kusc.jpeg")
-        # Resize the image
-        img = img.resize((80, 50), Image.ANTIALIAS)
-        # Convert the image to PhotoImage object
-        self.bg_image = ImageTk.PhotoImage(img)
-        self.image_label = Label(self.window, image=self.bg_image)
-        self.image_label.place(x=120, y=50)
+# ============================ NPR Buttons  
+    
+        # Button to play NPR
+        self.play_button_npr = Button(self.window, text="Play NPR", command=self.play_npr, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.play_button_npr.grid(row=3, column=1, ipadx=0, ipady=0, sticky="ne")
 
-        self.stop_button_kusc_classical = Button(self.window, text="Stop ", command=self.stop_kusc_classical)
-        self.stop_button_kusc_classical.place(x=240, y=60)
+# Load the image
+        npr_image = Image.open("images/npr.png")
+        npr_image = npr_image.resize((60, 30), Image.ANTIALIAS)
+        self.npr_image = ImageTk.PhotoImage(npr_image)
+        self.npr_image_label = Label(self.window, image=self.npr_image, borderwidth=0, highlightthickness=0)
+        self.npr_image_label.grid(row=3, column=2, ipadx=0, ipady=0, sticky="n")
+        
+        # Button to stop NPR
+        self.stop_button_npr = Button(self.window, text="Stop NPR", command=self.stop_npr, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.stop_button_npr.grid(row=3, column=3, ipadx=0, ipady=0, sticky="nw")
 
-         # Button to play and pause MN Classical
-        self.play_button_mn_classical = Button(self.window, text="Play MN", command=self.play_mn_classical)
-        self.play_button_mn_classical.place(x=40, y=120)
-        self.stop_button_mn_classical = Button(self.window, text="Stop MN", command=self.stop_mn_classical)
-        self.stop_button_mn_classical.place(x=240, y=120)
+# ============================ BBC Buttons  
+    
+        # Button to play BBC
+        self.play_button_bbc = Button(self.window, text="Play BBC", command=self.play_bbc, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.play_button_bbc.grid(row=4, column=1, ipadx=0, ipady=0, sticky="ne")
 
-        # Button to play and pause NPR.org NPR
-        self.play_button_npr = Button(self.window, text="Play NPR", command=self.play_npr)
-        self.play_button_npr.place(x=40, y=160)
-        self.stop_button_npr = Button(self.window, text="Stop NPR", command=self.stop_npr)
-        self.stop_button_npr.place(x=240, y=160)
+# Load the image
+        bbc_image = Image.open("images/bbc.png")
+        bbc_image = bbc_image.resize((60, 30), Image.ANTIALIAS)
+        self.bbc_image = ImageTk.PhotoImage(bbc_image)
+        self.bbc_image_label = Label(self.window, image=self.bbc_image, highlightthickness=0)
+        self.bbc_image_label.grid(row=4, column=2, ipadx=0, ipady=0, sticky="n", )
 
+        # Button to stop BBC
+        self.stop_button_bbc = Button(self.window, text="Stop BBC", command=self.stop_bbc, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.stop_button_bbc.grid(row=4, column=3, ipadx=0, ipady=0, sticky="nw")
+        
+ # ============================ CNN Buttons  
+    
+        # Button to play CNN
+        self.play_button_cnn = Button(self.window, text="Play CNN", command=self.play_cnn, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.play_button_cnn.grid(row=5, column=1, ipadx=0, ipady=0, sticky="ne")
 
-        # Button to play and pause  BBC
-        self.play_button_bbc = Button(self.window, text="Play BBC", command=self.play_bbc)
-        self.play_button_bbc.place(x=40, y=200)
-        self.stop_button_bbc = Button(self.window, text="Stop BBC", command=self.stop_bbc)
-        self.stop_button_bbc.place(x=240, y=200)
-       
-        # Button to play and pause CNN
-        self.play_button_cnn = Button(self.window, text="Play CNN", command=self.play_cnn)
-        self.play_button_cnn.place(x=40, y=240)
-        self.stop_button_cnn = Button(self.window, text="Stop CNN", command=self.stop_cnn)
-        self.stop_button_cnn.place(x=240, y=240)
+# Load the image
+        cnn_image = Image.open("images/cnn.png")
+        cnn_image = cnn_image.resize((60, 30), Image.ANTIALIAS)
+        self.cnn_image = ImageTk.PhotoImage(cnn_image)
+        self.cnn_image_label = Label(self.window, image=self.cnn_image, highlightthickness=0)
+        self.cnn_image_label.grid(row=5, column=2, ipadx=0, ipady=0, sticky="n")
+
+       # Button to stop CNN
+        self.stop_button_cnn = Button(self.window, text="Stop CNN", command=self.stop_cnn, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.stop_button_cnn.grid(row=5, column=3, ipadx=0, ipady=0, sticky="nw")
 
 
  # Button to quit with command "self.close_browser" with function to close the browser window
-  
 
-        self.quit_button = Button(self.window, text="Quit", command=self.close_browser)
-        self.quit_button.place(x=120, y=300)
+        self.quit_button = Button(self.window, text="Quit", command=self.close_browser, bg="maroon", highlightbackground="maroon", highlightthickness=0)
+        self.quit_button.grid(row=6, column=2, ipadx=0, ipady=0, sticky="n")
 
  # TKinter event loop is started with "mainloop()"
         self.window.mainloop()
+
+
+# ====================== Webscraping online media players
 
 # Live streaming radio station websites to interact with play and stop or pause buttons
 
